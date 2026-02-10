d92e58f0f590f37a61a5cc1fc13c1705a86c4191
Diego Lima <lima.bcf@gmail.com>
2026-02-10 11:41:23 -0300

    Commit inicial

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+export function App() {
+  return (
+    <>
+      <h1>Olá Mundo!</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+        provident accusantium tempore assumenda voluptate rem commodi, magni ut.
+        Soluta impedit officia aperiam corporis ad quos culpa eligendi suscipit
+        fugiat ipsum.
+      </p>
+    </>
+  );
+}
