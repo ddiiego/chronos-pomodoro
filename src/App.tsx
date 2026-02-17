f4d04c88f86b3ef6547a5da499276ee96ff924f3
Diego Lima <lima.bcf@gmail.com>
2026-02-17 16:21:36 -0300

    Estrutura HTML e CSS, aula 21

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-//import './App.css';
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
+
+import { Container } from './components/Container';
+import { Heading } from './components/Heading';
 
 export function App() {
-  return (
-    <>
-      <Heading>Olá mundo!
-        <button>
-          <TimerIcon/>
-        </button>
-      </Heading>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio
-        provident accusantium tempore assumenda voluptate rem commodi, magni ut.
-        Soluta impedit officia aperiam corporis ad quos culpa eligendi suscipit
-        fugiat ipsum.
-      </p>
+  return (<>
+      <Container>
+        <Heading>LOGO</Heading>
+      </Container>
+
+      <Container>
+        <Heading>MENU</Heading>
+      </Container>
     </>
   );
 }
